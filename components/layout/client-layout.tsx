5a6684413ceb3f2d6011b686370f47bcccc409e5
SetFodi <lukafartenadze2004@gmail.com>
2025-04-30 14:32:21 +0400

    refactored the icon

--- components/layout/client-layout.tsx
+++ components/layout/client-layout.tsx
@@ -668,3 +668,15 @@ function SearchIcon() {
     </svg>
   );
 }
+function LogoutIcon() {
+  return (
+    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+      />
+    </svg>
+  );
+}
